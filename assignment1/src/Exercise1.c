898d165cac7f7d1335fa28e6fe597bae1b64ea38
Ziad <Ziadelhanafy12@gmail.com>
2023-10-17 18:51:05 +0300

    Assignment 1 part 1

---
+++ assignment1/src/Exercise1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Assignment1.c
+ Author      : Ziad ElHanafy
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//void main() {
+//	printf("C Programming");
+//	return;
+//}
---
+++ assignment1/src/Exercise5.c
@@ -0,0 +1,21 @@
+/*
+ * Exercise5.c
+ *
+ *  Created on: Oct 17, 2023
+ *      Author: zizob
+ */
+
+//#include <stdio.h>
+//#include <stdlib.h>
+////
+//void main() {
+//    char c;
+//
+//    printf("Enter a character: ");
+//    fflush(stdin);fflush(stdout);
+//    scanf("%c", &c);
+//    printf("ASCII value of %c = %d", c, c);
+//
+//    return;
+//}
+
