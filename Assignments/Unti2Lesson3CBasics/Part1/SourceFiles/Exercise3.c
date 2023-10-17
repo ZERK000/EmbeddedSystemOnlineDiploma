ec67a2379948698ff402b89599c8f00868b34258
Ziad <Ziadelhanafy12@gmail.com>
2023-10-17 19:26:55 +0300

    Adding Assignment 1 part 1

---
+++ Assignments/Unti2Lesson3CBasics/Part1/SourceFiles/Exercise3.c
@@ -0,0 +1,20 @@
+/*
+ * Excersise3.c
+ *
+ *  Created on: Oct 17, 2023
+ *      Author: zizob
+ */
+
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//void main() {
+//	int x, y;
+//	printf("Enter two integer: ");
+//	fflush(stdin);fflush(stdout);
+//	scanf("%d %d", &x , &y);
+//	printf("Sum: %d", x+y);
+//
+//	return;
+//}
